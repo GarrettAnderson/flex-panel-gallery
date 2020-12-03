dfac0fa3fd070d568b5c6abb12e8f324f08728d2
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-12-02 20:54:52 -0500

    updated npm and commented out previous code to work off a clean slate.

--- public/main.js
+++ public/main.js
@@ -1,16 +1,52 @@
-const panels = document.querySelectorAll('.panel')
 
-function toggleOpen() {
-  this.classList.toggle('open')
-}
+/* ==========================
+      GARRETT ANSWER
+   ========================== */
 
-function toggleActive(event) {
-  console.log(event.propertyName)
-  if (event.propertyName.includes('flex')) {
-    this.classList.toggle('open-active')
-  }
-}
 
-panels.forEach((panel) => panel.addEventListener('click', toggleOpen))
 
-panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==========================
+      WES BOS ANSWER
+   ========================== */
+
+
+
+// const panels = document.querySelectorAll('.panel')
+//
+// function toggleOpen() {
+//   this.classList.toggle('open')
+// }
+//
+// function toggleActive(event) {
+//   console.log(event.propertyName)
+//   if (event.propertyName.includes('flex')) {
+//     this.classList.toggle('open-active')
+//   }
+// }
+//
+// panels.forEach((panel) => panel.addEventListener('click', toggleOpen))
+//
+// panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive))
